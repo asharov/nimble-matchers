c82099c7296c87ab8ba142c9a1998d17f0d12f27
Jaakko Kangasharju <ashar@iki.fi>
2022-10-30 17:29:03 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,8 +14,8 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0-rc.3"),
-    .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0")
+    .package(url: "https://github.com/Quick/Nimble.git", from: "10.0.0"),
+    .package(url: "https://github.com/Quick/Quick.git", from: "4.0.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
